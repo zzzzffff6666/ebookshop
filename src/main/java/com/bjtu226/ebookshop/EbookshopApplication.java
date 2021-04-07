01e36f0f75c3f54d022e2f77dd1ba06a19c8fc3d
zhangfu <17770517023@163.com>
2021-04-07 17:11:24 +0800

    Initial commit

---
+++ src/main/java/com/bjtu226/ebookshop/EbookshopApplication.java
@@ -0,0 +1,13 @@
+package com.bjtu226.ebookshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EbookshopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EbookshopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bjtu226/ebookshop/EbookshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bjtu226.ebookshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EbookshopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
